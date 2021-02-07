736170760a612c7666c1dd09ff4e83102310bac7
Arve Hansen <arveh4@outlook.com>
2021-02-07 13:15:30 +0100

    Setting up SignalR server

---
+++ AHIsLearning.SignalR.Server/Hubs/NumberHub.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace AHIsLearning.SignalR.Server.Hubs
+{
+    public class NumberHub : Hub
+    {
+        public ChannelReader<int> GetNumber(int numberCount, CancellationToken cancellationToken)
+        {
+            var channel = Channel.CreateUnbounded<int>();
+
+            // We don't want to await WriteItemsAsync, otherwise we'd end up waiting 
+            // for all the items to be written before returning the channel back to
+            // the client.
+            _ = WriteNumberAsync(channel.Writer, numberCount, cancellationToken);
+
+            return channel.Reader;
+        }
+
+        private async Task WriteNumberAsync(ChannelWriter<int> channelWriter, int numberCount, CancellationToken cancellationToken)
+        {
+            Exception localException = null;
+            try
+            {
+                for (var i = 0; i < numberCount; i++)
+                {
+                    await channelWriter.WriteAsync(i, cancellationToken);
+                }
+            }
+            catch (Exception ex)
+            {
+                localException = ex;
+            }
+            finally
+            {
+                channelWriter.Complete(localException);
+            }
+        }
+    }
+}
--- AHIsLearning.SignalR.Server/Startup.cs
+++ AHIsLearning.SignalR.Server/Startup.cs
@@ -1,3 +1,4 @@
+using AHIsLearning.SignalR.Server.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -19,6 +20,7 @@ namespace AHIsLearning.SignalR.Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +48,7 @@ namespace AHIsLearning.SignalR.Server
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapHub<NumberHub>("/numbers");
             });
         }
     }
