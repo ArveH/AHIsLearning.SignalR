a43cfc8d2db403a9944563c7f73563b2b4651df6
Arve Hansen <arveh4@outlook.com>
2021-02-07 14:30:12 +0100

    Chat sample from MS doc

--- AHIsLearning.SignalR.BlazorClient/Startup.cs
+++ AHIsLearning.SignalR.BlazorClient/Startup.cs
@@ -1,15 +1,12 @@
 using AHIsLearning.SignalR.BlazorClient.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using AHIsLearning.SignalR.BlazorClient.Hubs;
+using Microsoft.AspNetCore.ResponseCompression;
 
 namespace AHIsLearning.SignalR.BlazorClient
 {
@@ -29,11 +26,18 @@ namespace AHIsLearning.SignalR.BlazorClient
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddResponseCompression(opts =>
+            {
+                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                    new[] { "application/octet-stream" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseResponseCompression(); 
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -53,6 +57,7 @@ namespace AHIsLearning.SignalR.BlazorClient
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapBlazorHub();
+                endpoints.MapHub<ChatHub>("/chathub");
                 endpoints.MapFallbackToPage("/_Host");
             });
         }
